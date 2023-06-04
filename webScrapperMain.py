c89564ff5b12e1d23312854b72dcf0c7d45be9d2
Eden Bar <93385056+Edenb2131@users.noreply.github.com>
2023-06-04 14:12:38 +0300

    Update webScrapperMain.py

--- webScrapperMain.py
+++ webScrapperMain.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 import argparse
 
 def telegram_bot_sendtext(bot_message):
-    bot_token = '1357917340:AAGZep2N_vyG_R_SHx1ZEMtqkd4xzwwE66o'
-    bot_chatID = '523361387'
+    bot_token = ************************************ # Here you enter you token 
+    bot_chatID = *********************************** # Here you enter you chatID number
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
